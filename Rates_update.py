73e7e4205e89ad871ee8d091ea74a252fbb5ba57
rdavill <robert.davill@kaiko.com>
2024-11-12 11:39:13 +0000

    Update Rates_update.py

--- Rates_update.py
+++ Rates_update.py
@@ -12,19 +12,21 @@ def pull_and_save_data_to_csv(api_url):
         items = []
         for item in data['data']:
             ticker = item['ticker']
+            brand = item['brand']
             quote_short_name = item['quote']['short_name'].upper()
             base_short_name = item['base']['short_name'].upper()
             type = item['type'].replace('_', ' ')
             dissemination = item['dissemination']
             short_name = item['short_name'].replace('_', ' ')
+            launch_date = datetime.strptime(item['launch_date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%B %d, %Y')
             inception = datetime.strptime(item['inception_date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%B %d, %Y')
 
-            items.append((ticker, quote_short_name, base_short_name, type, dissemination, short_name, inception))
+            items.append((ticker, brand, quote_short_name, base_short_name, type, dissemination, short_name, launch_date, inception))
 
-        headers = ['Ticker', 'Quote (short name)', 'Base (short name)', 'Type', 'Dissemination', 'Rate Short name', 'Inception']
+        headers = ['Ticker','Brand', 'Quote (short name)', 'Base (short name)', 'Type', 'Dissemination', 'Rate Short name', 'Launch Date', 'Inception']
         sorted_items = sorted(items, key=lambda row: row[2])
 
-        with open("Reference_Rates_Coverage.csv", "w") as csv_file:  # Simplified path
+        with open("Reference_Rates_Coverage.csv", "w") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(headers)
             writer.writerows(sorted_items)
