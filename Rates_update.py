270645ee078cd0041dbaf28e55cd2589c72dc718
rdavill <robert.davill@kaiko.com>
2024-12-17 15:45:02 +0000

    Update Rates_update.py

--- Rates_update.py
+++ Rates_update.py
@@ -3,12 +3,18 @@ import json
 import csv
 from datetime import datetime
 
+def parse_date(date_string):
+    try:
+        # Try parsing with milliseconds
+        return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d, %Y')
+    except ValueError:
+        # If that fails, try without milliseconds
+        return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ').strftime('%B %d, %Y')
 
 def pull_and_save_data_to_csv(api_url):
     response = requests.get(api_url)
     if response.status_code == 200:
         data = json.loads(response.text)
-
         items = []
         for item in data['data']:
             ticker = item['ticker']
@@ -18,14 +24,12 @@ def pull_and_save_data_to_csv(api_url):
             type = item['type'].replace('_', ' ')
             dissemination = item['dissemination']
             short_name = item['short_name'].replace('_', ' ')
-            launch_date = datetime.strptime(item['launch_date'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d, %Y')
-            inception = datetime.strptime(item['inception_date'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d, %Y')
-
+            launch_date = parse_date(item['launch_date'])
+            inception = parse_date(item['inception_date'])
             items.append((ticker, brand, quote_short_name, base_short_name, type, dissemination, short_name, launch_date, inception))
-
+        
         headers = ['Ticker','Brand', 'Quote (short name)', 'Base (short name)', 'Type', 'Dissemination', 'Rate Short name', 'Launch Date', 'Inception']
         sorted_items = sorted(items, key=lambda row: row[3])
-
         with open("Reference_Rates_Coverage.csv", "w") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(headers)
